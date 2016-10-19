169441663ab4728bf45685d5c4ab6fe7c5744717
aobi <aobichen@gmail.com>
2016-10-19 20:30:02 +0800

    first commit

---
+++ WGHotel/Areas/Backend/RouteConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace WGHotel.Areas.Backend
+{
+    internal static class RouteConfig
+    {
+        internal static void RegisterRoutes(AreaRegistrationContext context)
+        {
+            //routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            context.MapRoute(
+               "Backend_default",
+               "Backend/{controller}/{action}/{id}",
+               new { action = "Index", id = UrlParameter.Optional }
+           );
+        }
+    }
+}
